5096eb790bd487219a736d866a3b380d30d53d82
chrisrulz112 <chrisrulz112@gmail.com>
2026-04-12 13:35:39 +1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'training-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './sw.js'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
